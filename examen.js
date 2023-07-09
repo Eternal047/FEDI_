192b73d83a148ec87e21ebe79a958d49751493ae
Luna <kelly.bellido@unmsm.edu.pe>
2023-07-09 03:24:03 -0500

    a

--- examen.js
+++ examen.js
@@ -132,7 +132,7 @@ function calcularResultado() {
     // Pregunta 1
     var q1 = document.getElementsByName("q1");
     for (var i = 0; i < q1.length; i++) {
-      if (q1[i].checked && q1[i].value === "d") {
+      if (q1[i].checked && q1[i].value === "b") {
         puntuacion++;
       }
     }
@@ -140,7 +140,7 @@ function calcularResultado() {
     // Pregunta 2
     var q2 = document.getElementsByName("q2");
     for (var j = 0; j < q2.length; j++) {
-      if (q2[j].checked && q2[j].value === "b") {
+      if (q2[j].checked && q2[j].value === "a") {
         puntuacion++;
       }
     }
@@ -148,7 +148,7 @@ function calcularResultado() {
     // Pregunta 3
     var q3 = document.getElementsByName("q3");
     for (var j = 0; j < q3.length; j++) {
-      if (q3[j].checked && q3[j].value === "a") {
+      if (q3[j].checked && q3[j].value === "d") {
         puntuacion++;
       }
     }
@@ -156,7 +156,7 @@ function calcularResultado() {
     // Pregunta 4
     var q4 = document.getElementsByName("q4");
     for (var j = 0; j < q4.length; j++) {
-      if (q4[j].checked && q4[j].value === "a") {
+      if (q4[j].checked && q4[j].value === "b") {
         puntuacion++;
       }
     }
@@ -164,7 +164,7 @@ function calcularResultado() {
     // Pregunta 5
     var q5 = document.getElementsByName("q5");
     for (var j = 0; j < q3.length; j++) {
-      if (q5[j].checked && q5[j].value === "c") {
+      if (q5[j].checked && q5[j].value === "a") {
         puntuacion++;
       }
     }
@@ -172,7 +172,7 @@ function calcularResultado() {
     // Pregunta 6
     var q6 = document.getElementsByName("q6");
     for (var j = 0; j < q6.length; j++) {
-      if (q6[j].checked && q6[j].value === "b") {
+      if (q6[j].checked && q6[j].value === "c") {
         puntuacion++;
       }
     }
@@ -180,7 +180,7 @@ function calcularResultado() {
     // Pregunta 7
     var q7 = document.getElementsByName("q7");
     for (var j = 0; j < q7.length; j++) {
-      if (q7[j].checked && q7[j].value === "d") {
+      if (q7[j].checked && q7[j].value === "b") {
         puntuacion++;
       }
     }
